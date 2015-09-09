bf81dabf67ede731106603254eb944f9434d97ef
Meng Lu <menglu@email.arizona.edu>
2015-09-09 11:05:33 -0700

    homework2

--- homework2/homework2.pl
+++ homework2/homework2.pl
@@ -3,16 +3,14 @@
 use strict;
 use warnings;
 print "Hello Tucson\n";
-my $NAME = MengLu;
+my $NAME = "MengLu";
 print "My name is $NAME\n";
 print "This is\tthe Metagenomics Course\n";
 print "2*6 =", 2*6, "\n";
-my $State1 = Math question;
-print 'State1:\t 2+3 = 2+3';
+my $State1 = "Math question";
+print "'State1:\t 2+3 = 2+3',\n";
 print "State1:\t2+3 =",2+3, "\n";
-
-my $Question = What is the difference between single and double quotes?;
-print "Question:\n Double quotes use variable expansion\n but Single quotes do not use variable expansion"
+print "Double quotes use variable expansion, but Single quotes do not use variable expansion\n";
 
 
 
