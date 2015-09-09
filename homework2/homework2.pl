d2308879e2c630c64a5d6348933d035bd31b3d04
Meng Lu <menglu@email.arizona.edu>
2015-09-09 09:49:51 -0700

    homework2

---
+++ homework2/homework2.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+# file: homework2.pl
+use strict;
+use warnings;
+print "Hello Tucson\n";
+my $NAME = MengLu;
+print "My name is $NAME\n";
+print "This is\tthe Metagenomics Course\n";
+print "2*6 =", 2*6, "\n";
+my $State1 = Math question;
+print 'State1:\t 2+3 = 2+3';
+print "State1:\t2+3 =",2+3, "\n";
+
+my $Question = What is the difference between single and double quotes?;
+print "Question:\n Double quotes use variable expansion\n but Single quotes do not use variable expansion"
+
+
+
+
