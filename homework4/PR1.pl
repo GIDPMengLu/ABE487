50382fa601733a39c813c92f6fa8991966a64038
Ken Youens-Clark <kyclark@gmail.com>
2015-10-05 15:48:53 -0700

    suggestions

--- homework4/PR1.pl
+++ homework4/PR1.pl
@@ -1,32 +1,46 @@
-#!usr/bin/perl
+#!/usr/bin/env perl
 
 use strict;
 use warnings;
 
-
 my $num1 = shift;
 my $num2 = shift;
 
-my $err = "err.txt";
+my $err    = "err.txt";
 my $output = "out.txt";
 
-open (OUTFILE, '>', $output);
-open (STDERR, '>', $err);
+#open (OUTFILE, '>', $output);
+#open (STDERR, '>', $err);
 
-if(!defined($num1) or !defined($num2)){
-print "Please type two real numbers\n";
+if (!defined($num1) or !defined($num2)) {
+    print "Please type two real numbers\n";
 }
-else{
-if($num1<0 or $num2<0){
-print "The numbers have to be positive";
+else {
+    if ($num1 < 0 or $num2 < 0) {
+        print "The numbers have to be positive";
+    }
+    else {
+        if ($num2 == 0) {
+            print "The divisor cannot be zero";
+        }
+        else {
+            my $cal = $num1 / $num2;
+            print $cal, "\n";
+        }
+    }
 }
-else{
-if($num2 == 0){
-print "The divisor cannot be zero";
-}
-else{
-my $cal = $num1/$num2;
-print OUTFILE $cal,"\n";
-}
-}
-}
+
+__END__
+
+Don't open STDOUT/STDERR, just print to the appropriate handled.  Let the 
+user sort it out.  
+
+  $ program.pl 1>out 2>err
+
+Half off.
+
+Please format your code with proper indentation.  Use the "perltidy" 
+program if you need:
+
+[hpc:service0@~]$ type perltidy
+perltidy is /rsgrps/bhurwitz/hurwitzlab/perl5/bin/perltidy
--- homework4/PR2.pl
+++ homework4/PR2.pl
@@ -1,17 +1,24 @@
-#!usr/bin/perl
+#!/usr/bin/env perl
+
 use strict;
 use warnings;
+use autodie;
 
-
-my $in = 'Perl_III_nobody.txt';
+my $in  = 'Perl_III_nobody.txt';
 my $out = 'upper.out';
 
-open(IN, '<', $in);
-open(OUT, '>', $out);
+open my $IN,  '<', $in;
+open my $OUT, '>', $out;
 
-while(my $line = <IN>){
-chomp($line);
-my $upperline = uc($line);
-print OUT "upperline\n";
+while (my $line = <$IN>) {
+    my $upperline = uc($line);
+    print OUT "upperline";
 }
 
+__END__
+
+Either "use autodie" or be sure to check the result of your "open"!
+
+Never use global symbols for you file handles! Use scalars!
+
+Don't chomp if you're going to print the newline.
